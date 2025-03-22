e87ce49254c0b099c76a293711d12b7a8de55859
Guido Günther <agx@sigxcpu.org>
2025-03-22 08:47:56 +0100

    treewide: Release 0.0.2
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 #[allow(dead_code)]
-pub static VERSION: &str = "0.0.1";
+pub static VERSION: &str = "0.0.2";
 pub static GETTEXT_PACKAGE: &str = "pfs";
 pub static LOG_DOMAIN: &str = "pfs";
 pub static LOCALEDIR: &str = "/usr/local/share/locale";
